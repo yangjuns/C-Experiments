4353a7478cda07d53385590de9c7707e84fee707
Yangjun Sheng <yangjuns@andrew.cmu.edu>
2019-04-24 17:45:51 -0400

    Add info message

--- thread.cpp
+++ thread.cpp
@@ -3,16 +3,18 @@
 #include <thread>
 
 int sum = 0;
-void Add() {
+void Add(uint32_t id) {
+  std::cout << "Start thread " << id << std::endl;
   for (int i = 0; i < 1000000; i++)
     sum++;
+  std::cout << "End thread " << id << std::endl;
 }
 
 void threadCaller() {}
 
 int main() {
-  std::thread t1(Add);
-  std::thread t2(Add);
+  std::thread t1(Add, 1);
+  std::thread t2(Add, 2);
   t1.join();
   t2.join();
   std::cout << sum << std::endl;
